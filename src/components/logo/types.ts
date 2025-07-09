a08b3b2407eae4801f7007029ca40b522f9fe5dd
Sebastian Pietras <me@spietras.dev>
2025-07-09 18:19:36 +0000

    Fixed `Logo` component

--- src/components/logo/types.ts
+++ src/components/logo/types.ts
@@ -3,7 +3,7 @@ import {
   PolymorphicComponentProps,
 } from "@mantine/core";
 
-export type BaseLogoInput = Omit<MantineImageProps, "src">;
+export type BaseLogoInput = MantineImageProps;
 
 export type LogoInput<C extends React.ElementType = "img"> =
   PolymorphicComponentProps<C, BaseLogoInput>;
