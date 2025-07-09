43fd91b77fb6eb2bcd65d2647a37c2c1c48c0d4a
Sebastian Pietras <me@spietras.dev>
2025-07-09 20:23:08 +0200

    Fixed `Logo` component (#31)

--- src/components/logo/main.tsx
+++ src/components/logo/main.tsx
@@ -10,18 +10,25 @@ import { BaseLogoInput } from "./types";
 /** Component for displaying the logo */
 export const Logo = createPolymorphicComponent<"img", BaseLogoInput>(
   forwardRef<HTMLImageElement, BaseLogoInput>(function Logo(
-    { fit = "contain", flex = "initial", mah = "100%", maw = "100%", ...props },
+    {
+      fit = "contain",
+      flex = "initial",
+      mah = "100%",
+      maw = "100%",
+      src = logo,
+      ...props
+    },
     ref,
   ) {
     return (
       <MantineImage
-        {...props}
         fit={fit}
         flex={flex}
         mah={mah}
         maw={maw}
         ref={ref}
-        src={logo}
+        src={src as unknown}
+        {...props}
       />
     );
   }),
